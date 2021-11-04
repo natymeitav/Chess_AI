c47ea22e39ef4ee222916ea3268284b022350404
naty <naty.meitav@Gmail.com>
2021-11-04 18:18:41 +0200

    Fix Knight getMoves.

--- pieces/Knight.py
+++ pieces/Knight.py
@@ -3,7 +3,7 @@ from Piece import Piece
 
 class Knight(Piece):
     def __init__(self, isWhite, pos, serial):
-        Piece.__init__(self, isWhite, pos, "N", serial)
+        self.init__ = Piece.__init__(self, isWhite, pos, "N", serial)
         self.value = 30 + 60 * (self.isWhite - 1)
 
     # check if knight can perform move
@@ -13,23 +13,24 @@ class Knight(Piece):
 
     # returns all potential moves
     def getMoves(self, logicBoard):
+        sub_moves = []
         moves = []
 
-        moves.append((self.pos[0] + 2, self.pos[1] + 1))
-        moves.append((self.pos[0] + 2, self.pos[1] - 1))
-        moves.append((self.pos[0] - 2, self.pos[1] + 1))
-        moves.append((self.pos[0] - 2, self.pos[1] - 1))
+        sub_moves.append((self.pos[0] + 2, self.pos[1] + 1))
+        sub_moves.append((self.pos[0] + 2, self.pos[1] - 1))
+        sub_moves.append((self.pos[0] - 2, self.pos[1] + 1))
+        sub_moves.append((self.pos[0] - 2, self.pos[1] - 1))
 
-        moves.append((self.pos[0] + 1, self.pos[1] + 2))
-        moves.append((self.pos[0] + 1, self.pos[1] - 2))
-        moves.append((self.pos[0] - 1, self.pos[1] + 2))
-        moves.append((self.pos[0] - 1, self.pos[1] - 2))
+        sub_moves.append((self.pos[0] + 1, self.pos[1] + 2))
+        sub_moves.append((self.pos[0] + 1, self.pos[1] - 2))
+        sub_moves.append((self.pos[0] - 1, self.pos[1] + 2))
+        sub_moves.append((self.pos[0] - 1, self.pos[1] - 2))
 
-        for move in moves:
-            if not (-1<move[0]<8 and -1<move[1]<8):
-                moves.remove(move)
-            elif logicBoard[move[0],move[1]] is not None:
-                  if logicBoard[move[0],move[1]].isWhite == self.isWhite:
-                      moves.remove(move)
+        for move in sub_moves:
+            if -1 < move[0] < 8 and -1 < move[1] < 8:
+                if logicBoard[move[0], move[1]] is None:
+                    moves.append(move)
+                elif logicBoard[move[0], move[1]].isWhite != self.isWhite:
+                    moves.append(move)
 
         return moves
