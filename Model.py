a983d06f9111a9f467cd5bf5df27f872eb478152
naty meitav <naty.meitav@Gmail.com>
2021-11-07 16:36:53 +0200

    Add GetMax and GetMin.

--- Model.py
+++ Model.py
@@ -12,15 +12,9 @@ class CPU:
                     temp_logic = copy.deepcopy(logic)
                     temp_piece = copy.deepcopy(piece)
 
-                    # check for capture
-                    if temp_logic[move[0], move[1]] is not None:
-                        pieces[temp_logic[move[0], move[1]].serialNum] = None
-
                     # update boards
                     temp_logic[move[0], move[1]] = temp_piece
                     temp_logic[temp_piece.pos[0], temp_piece.pos[1]] = None
-
-                    print(temp_logic[move[0], move[1]])
 
                     # update piece's first move
                     if temp_logic[move[0], move[1]].firstMove:
@@ -29,14 +23,83 @@ class CPU:
                     # update piece's position
                     temp_logic[move[0], move[1]].pos = move
 
-                    boards.append(temp_logic)
+                    boards.append((temp_logic,temp_logic[move[0], move[1]].pos))
 
         return boards
 
+    # TODO:
+    # update captured pieces
+    # fix MakeMove
+
     @staticmethod
     def MakeMove(logic, black, white):
+        max_val = float('-inf')
+        max_board = None
+        max_piece = None
         for board in CPU.getBoards(logic, black):
-            CPU.printBoard(board)
+            print("a")
+            value = CPU.getMin(board[0], black, white, 1)
+            if value > max_val:
+                max_val = value
+                max_board = board
+                max_piece = board[1]
+
+        return max_board, max_piece
+
+
+    @staticmethod
+    def getMax(logic, black, white, depth):
+
+        if depth == 3:
+            return 0
+
+        max_val = float('-inf')
+
+        endgame = CPU.checkEndGame(black,white)
+        if endgame != -999:
+            print("endgame "+ str(endgame))
+            return endgame
+
+        for board in CPU.getBoards(logic, black):
+            value = CPU.getMin(board[0], black, white, depth+1)
+            if value > max_val:
+                max_val = value
+        return max_val
+
+    @staticmethod
+    def getMin(logic, black, white, depth):
+
+        if depth == 3:
+            return 0
+
+        min_val = float('inf')
+        endgame = CPU.checkEndGame(black, white)
+        if endgame != -999:
+            print("endgame " + str(endgame))
+            return endgame
+        for board in CPU.getBoards(logic,white):
+            board = board[0]
+            value = CPU.getMax(board[0], black, white, depth+1)
+            if value < min_val:
+                min_val = value
+        return min_val
+
+
+    # check for win or tie
+    @staticmethod
+    def checkEndGame(black,white):
+        # check for white win
+        endgame = -999
+        if str(black[4]) != "K0":
+            endgame = 1
+        # check for black win
+        elif str(white[12]) != "K1":
+            endgame = -1
+        # check for insufficient material
+        elif len(set(white + black)) == 3:
+            endgame = 0
+
+        return endgame
 
     @staticmethod
     def printBoard(listLogicBoard):
