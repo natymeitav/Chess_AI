a5b29bf933ced2653fb03b4dcd60f426463715d7
natymeitav <naty.meitav@Gmail.com>
2022-02-02 09:29:40 +0200

    castling

--- pieces/Rook.py
+++ pieces/Rook.py
@@ -11,19 +11,15 @@ class Rook(Piece):
     # check if rook can perform move
     def canMakeMove(self, new, logicBoard):
         if self.pos[0] == new[0] and self.pos[1] < new[1]:  # right to new position
-            self.firstMove = False
             return np.all(logicBoard[self.pos[0], self.pos[1] + 1:new[1]] == None)
 
         elif self.pos[0] == new[0] and self.pos[1] > new[1]:  # left to new position
-            self.firstMove = False
             return np.all(logicBoard[self.pos[0], new[1] + 1:self.pos[1]] == None)
 
         elif self.pos[0] < new[0] and self.pos[1] == new[1]:  # under new position
-            self.firstMove = False
             return np.all(logicBoard[self.pos[0] + 1:new[0], self.pos[1]] == None)
 
         elif self.pos[0] > new[0] and self.pos[1] == new[1]:  # over new position
-            self.firstMove = False
             return np.all(logicBoard[new[0] + 1:self.pos[0], self.pos[1]] == None)
 
         return False
